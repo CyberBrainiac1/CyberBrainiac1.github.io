c3e266382cc9361616d1ac14973cbcca1f8b2abc
Pranav <85799791+CyberBrainiac1@users.noreply.github.com>
2026-05-28 21:37:02 -0700

    Add Servo Control PWA service worker

---
+++ servocontrol/sw.js
@@ -0,0 +1,43 @@
+const cacheName = "servo-control-v1";
+const appShell = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(appShell))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== cacheName)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).catch(() => caches.match("./index.html"));
+    })
+  );
+});
